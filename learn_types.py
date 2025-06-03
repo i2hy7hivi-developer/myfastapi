b279166b1d4cd11c4deb7d29914d45e0db2f645c
Rhythm Dubey <i2hy7hivi.developer@gmail.com>
2025-06-03 06:33:38 +0530

    Python types

---
+++ learn_types.py
@@ -0,0 +1,8 @@
+def get_full_name(first_name: str, last_name: str) -> str:
+    full_name = f"My name is: {first_name.capitalize()} {last_name.capitalize()}"
+    return full_name
+
+print(get_full_name("john", "doe"))
+
+# This is a simple print statement
+# print("Good Morning")
